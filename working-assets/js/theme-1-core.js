b70799eb5ee10fc8d272c7a43458fd12e5abe4f4
Adam Snape <adam.snape@gymking.com>
2025-07-04 19:42:49 +0100

    add the nav drawer open/close functionality

--- working-assets/js/theme-1-core.js
+++ working-assets/js/theme-1-core.js
@@ -4,12 +4,55 @@ if (!customElements.get("slide-drawer")) {
     class SlideDrawer extends HTMLElement {
       constructor() {
         super();
+        this.side = this.getAttribute("side");
       }
 
       connectedCallback() {
-        const side = this.getAttribute('side');
-        this.classList.add(side);
+        this.classList.add(this.side);
+      }
 
+      open() {
+        this.classList.remove(this.side);
+        document.body.classList.add("no-scroll");
+        this.setAttribute("open", "");
+      }
+
+      close() {
+        this.classList.add(this.side);
+        document.body.classList.remove("no-scroll");
+        this.removeAttribute("open");
+      }
+    }
+  );
+}
+
+document.querySelectorAll(".drawer-button").forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const drawer = document.getElementById(
+      event.target.closest("button").getAttribute("data-target")
+    );
+    drawer.open();
+  });
+});
+
+if (!customElements.get("page-overlay")) {
+  customElements.define(
+    "page-overlay",
+    class PageOverlay extends HTMLElement {
+      connectedCallback() {
+
+        this.addEventListener("click", this._closeAll.bind(this));
+      }
+
+      _closeAll() {
+        document.body.classList.remove("no-scroll");
+        const drawers = document.querySelectorAll("slide-drawer");
+        drawers.forEach((drawer) => {
+          let side = drawer.getAttribute("side");
+          if (!drawer.classList.contains(side)) {
+            drawer.close();
+          }
+        });
       }
     }
   );
