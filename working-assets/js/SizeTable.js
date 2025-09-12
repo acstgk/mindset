bd154579dca70917e283b258d60fff59a24742ad
Adam Snape <adam.snape@gymking.com>
2025-09-12 14:39:50 +0100

    add all the womens size guides to pdp

---
+++ working-assets/js/SizeTable.js
@@ -0,0 +1,66 @@
+if (!customElements.get("size-table")) {
+  customElements.define(
+    "size-table",
+    class SizeGuideTable extends HTMLElement {
+      constructor() {
+        super();
+        this.toggleView = this.toggleView.bind(this);
+        this.toggleVisibility = this.toggleVisibility.bind(this);
+      }
+
+      //set up logic
+      connectedCallback() {
+        this.table = this.querySelector("table");
+        this.cms = this.querySelectorAll(".cm");
+        this.ins = this.querySelectorAll(".in");
+        this.units = this.querySelectorAll(".unit");
+        this.toggleVisibility(this.ins);
+        this.toggleVisibility(this.units);
+        this.addToggleControls();
+      }
+
+      // utility method to toggle element visibilityu
+      toggleVisibility(elements) {
+        elements.forEach((element) => {
+          element.classList.toggle("visually-hidden");
+        });
+      }
+
+      // utility method for creating the toggle control buttons
+      createButton(label, className, onClick) {
+        const button = document.createElement("button");
+        button.textContent = label;
+        button.classList.add(className, "button");
+        button.addEventListener("click", onClick);
+        return button;
+      }
+
+      // add the toggle control buttons to the DOM
+      addToggleControls() {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("toggle-buttons");
+
+        const button1 = this.createButton("cm", "button-primary", this.toggleView);
+        const button2 = this.createButton("inches", "button-secondary", this.toggleView);
+
+        newDiv.appendChild(button1);
+        newDiv.appendChild(button2);
+
+        this.table.parentNode.insertBefore(newDiv, this.table);
+      }
+
+      // this is the function that is called on toggle control button click.
+      toggleView(e) {
+        if (e.target.classList.contains("button-secondary")) {
+          const buttons = this.querySelectorAll(".button");
+          buttons.forEach((button) => {
+            button.classList.toggle("button-primary");
+            button.classList.toggle("button-secondary");
+          });
+          this.toggleVisibility(this.ins);
+          this.toggleVisibility(this.cms);
+        }
+      }
+    },
+  );
+}
--- working-assets/js/main.js
+++ working-assets/js/main.js
@@ -775,6 +775,7 @@ new ComponentLoader("product-recommendations", () => import("./ProductRecommenda
 new ComponentLoader("personal-recommendations", () => import("./PersonalisedCarousel.js"));
 new ComponentLoader("content-accordian", () => import("./ContentAccordian.js"));
 new ComponentLoader("bnpl-options", () => import("./BnplOptions.js"));
+new ComponentLoader("size-table", () => import("./SizeTable.js"));
 
 // with product cards in the cart drawer, always load the product-card module even without the scroll listener/observer
 if (!customElements.get("product-card")) {
