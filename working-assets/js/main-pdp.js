7751a480a871edd38fc308b0b2a42a2b8583c853
Adam Snape <adam.snape@gymking.com>
2025-11-05 13:09:46 +0000

    accessibility improvements

--- working-assets/js/main-pdp.js
+++ working-assets/js/main-pdp.js
@@ -24,6 +24,7 @@ if (!customElements.get("pdp-carousel")) {
         this.splide = null;
         this.zoomBtn = document.createElement("button");
         this.zoomBtn.className = "pdp-zoom-btn round-btn";
+        this.zoomBtn.ariaLabel = "Open Zoom Modal";
         this.zoomBtn.innerHTML = `  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M7 10l6 0" /><path d="M10 7l0 6" /><path d="M21 21l-6 -6" /></svg>`;
         this.zoomBtn.addEventListener("click", () => this._zoom());
         this.zoomEl = document.querySelector(".pdp-zoom-wrapper");
