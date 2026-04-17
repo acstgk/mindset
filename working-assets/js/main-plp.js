271c4d4ce35b0634c6f9857d8020864f0d4e6c90
Adam Snape <adam.snape@gymking.com>
2026-04-17 10:08:28 +0100

    remove search page check on the search query updater on filters for dynamic collection pages

--- working-assets/js/main-plp.js
+++ working-assets/js/main-plp.js
@@ -226,7 +226,7 @@ class EnhancedFilters {
     let fetchUrl = url.toString();
 
     const params = new URLSearchParams(window.location.search);
-    if (params.has('q') && !url.search.includes('')) {
+    if (params.has('q')) {
       fetchUrl += `&q=${params.get('q')}`;
     }
 
