431fd59053908d77d9304c81ce4e7484e1d519d3
Adam Snape <adam.snape@gymking.com>
2026-07-30 14:14:13 +0100

    setup spotlight test

--- working-assets/js/shoplift-test.js
+++ working-assets/js/shoplift-test.js
@@ -2,10 +2,16 @@ function shopliftTest() {
   const hypothesisID = "019e8270-204a-74ac-92de-82e21cffdd21"; //the shopplift test ID
   const style = document.createElement("style");
   window.shoplift.isHypothesisActive(hypothesisID).then((active) => {
-    if (active) {
+    if (active && window.theme.pageType == "index") {
       console.log("SHOPLIFT:: Active");
       style.textContent = `
-        .cart_items-ctl { display: none !important; height: 0; width: 0;}
+        .yotpo-widget-loyalty-floater-widget.yotpo-widget-override-css {
+            display: none;
+        }
+
+        #shopify-section-template--28979726614911__1785416516408442e1 {
+          display: block;
+      }
     `;
       document.head.appendChild(style);
     }
